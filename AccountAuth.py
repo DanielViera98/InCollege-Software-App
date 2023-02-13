11577eeb50007c4102044e5fd2e14c53ee6058ea
Tan <35444054+tanthai028@users.noreply.github.com>
2023-02-13 17:42:22 -0500

    sep file for auth

--- AccountAuth.py
+++ AccountAuth.py
@@ -1,2 +1,37 @@
 ### Handles checking if registered password is secure ###
 
+def verify_login_info(data, username, password):
+  if username in data:
+    return data[username]['password'] == password
+
+  else:
+    return False
+
+def has_capital_letter(password):
+  for char in password:
+    if char.isupper(): return True
+  return False
+
+def has_min_char(password):
+  return len(password) >= 8
+
+def has_max_char(password):
+  return len(password) <= 12
+
+def has_digit(password):
+  for char in password:
+    if char.isdigit(): return True
+  return False
+
+def has_special_char(password):
+  for char in password:
+    if not char.isalnum(): return True
+  return False
+
+def is_secure_password(password):
+  return has_capital_letter(password) and\
+    has_min_char(password) and\
+    has_max_char(password) and\
+    has_max_char(password) and\
+    has_digit(password) and\
+    has_special_char(password)
