697b514e957def97f503d330c51fd41183a427b2
Craigs12 <craigsankar@gmail.com>
2023-02-12 19:57:40 -0500

    updated to add job posting feature

--- App.py
+++ App.py
@@ -11,8 +11,9 @@ class InCollege:
 		 "Learn C", "Learn C#", "Learn Python", "Learn Java", "Learn HTML"
 		]
 		self.menu_options = ["Login","Why join InCollege", "Register"]
-		self.options = ["Search for jobs", "Find someone you know", "Learn a new skill"]
+		self.options = ["Search for Jobs/Internships", "Find someone you know", "Learn a new skill"]
 		self.system = AccountSystem()
+		self.jobs_options = ["Search for Jobs/Internships", "Post Job"]
 
 	# Helper method to print options
 	def print_options(self, options, label="Back"):
@@ -62,7 +63,7 @@ class InCollege:
 			option = int(input("> "))
 			match option:
 				case 1:
-					self.search_jobs()
+					self.show_job_options()
 				case 2:
 					self.network()
 				case 3:
@@ -70,26 +71,59 @@ class InCollege:
 				case 4:
 					return
 
+	def show_job_options(self):
+		option = -1
+
+		while option !=3:
+			os.system("clear")
+			print("Choose a task:\n")
+			self.print_options(self.jobs_options)
+
+			option = int(input("> "))
+			match option:
+				case 1:
+					self.search_jobs()
+				case 2:
+					self.job_updater()
+				case 3:
+					return	
 	# Handles job searches
 	def search_jobs(self):
-		self.post_jobs()
 		input("\nUnder construction...")
+  
   # Handles job posting
-	def post_jobs(username,job_title,description,employer,location,salary):
+	def post_jobs(self,username,job_title,description,employer,location,salary):
 		with open('job_postings.json', 'r') as file:
-		  jobs = json.load(file)
+			jobs = json.load(file)
 
-    #adding a new job
+    	#adding a new job
 		jobs[username] = {
-    "title": job_title,
-    "description": description,
-    "employer": employer,
-    "location": location,
-    "salary": salary
-    }
-  # Write the updated data back to the file
+		"title": job_title,
+		"description": description,
+		"employer": employer,
+		"location": location,
+		"salary": salary
+		}
+		# Write the updated data back to the file
 		with open('job_postings.json', 'w') as file:
-		  json.dump(jobs, file, indent=2)
+			json.dump(jobs, file, indent=2)
+
+	def job_updater(self):
+		success = False
+
+		while success==False:
+			username = input("Username: ")
+			job_title = input("Job Title: ")
+			description = input("Description: ")
+			employer = input("Employer: ")
+			location = input("Location: ")
+			salary = input("salary: ")
+
+			self.post_jobs(username,job_title,description,employer,location,salary)
+			success = True
+		
+		return success
+
 
 	# Handles networking
 	def network(self):
