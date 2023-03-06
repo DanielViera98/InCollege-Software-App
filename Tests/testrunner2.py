f1727548a4d3d63cc922e5d13cb4fa03b9bce866
HeWhoWalksBefore <dviera@usf.edu>
2023-03-06 13:01:01 -0500

    Added Tests/Results to .gitignore and fixed testrunner2.py

--- Tests/testrunner2.py
+++ Tests/testrunner2.py
@@ -1,19 +1,20 @@
 import pytest
 from AccountSystem import AccountSystem
 from io import StringIO
-
+from MetaTests import empty_students
 system = AccountSystem()
 
-#Test register function for atleast 6 entries
-input = (("D1\nI234567*\nA\na\n\n", True), ("D2\nI234567*\nB\nb\n\n", True),
-         ("D3\nI234567*\nC\nc\n\n", True), ("D4\nI234567*\nD\nd\n\n", True),
-         ("D5\nI234567*\nE\ne\n\n", True), ("D6\nI234567*\nF\nf\n\n", False))
-
-
+empty_students()
+#Test register function for atleast 10 entries
+input = (("Account1\nI234567*\nA\na\n\n", True), ("Account2\nI234567*\nB\nb\n\n", True),
+         ("Account3\nI234567*\nC\nc\n\n", True), ("Account4\nI234567*\nD\nd\n\n", True),
+         ("Account5\nI234567*\nE\ne\n\n", True), ("Account6\nI234567*\nF\nf\n\n", True),
+         ("Account7\nI234567*\nA\na\n\n", True), ("Account8\nI234567*\nB\nb\n\n", True),
+         ("Account9\nI234567*\nC\nc\n\n", True), ("Account10\nI234567*\nD\nd\n\n", True),
+         ("Account11\nI234567*\nC\nc\n\n", False))
 def test_register_max_entries(capsys, monkeypatch):
 	for i, r in input:
 		print(i, r)
 		test = StringIO(i)
 		monkeypatch.setattr('sys.stdin', test)
 		assert system.register() == r
-
