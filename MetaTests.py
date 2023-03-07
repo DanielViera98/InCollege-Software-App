7bfbf1d4e1f92276f2673791a9299f832be5966e
Daniel Viera <dviera98@gmail.com>
2023-03-07 17:12:50 -0500

    Revert "test"

--- MetaTests.py
+++ MetaTests.py
@@ -2,19 +2,27 @@
 import os
 import subprocess
 
-os.system('clear')
+#Open Results files
+file1 = open("Tests/Results/Runner1.txt", "w+")
+file2 = open("Tests/Results/Runner2.txt", "w+")
+file3 = open("Tests/Results/Runner3.txt", "w+")
+file3 = open("Tests/Results/Jobs.txt", "w+")
+file3 = open("Tests/Results/Search_someone.txt", "w+")
+file3 = open("Tests/Results/Jobs_max.txt", "w+")
+os.system('clear');
 
 def empty_students():
   student = open("students.json", "w")
   student.write("{\n}")
 def empty_jobs():
-  jobs = open("job_postings.json", "w")
-  jobs.write("{\n}")
+  student = open("job_postings.json", "w")
+  student.write("{\n}")
   
 empty_students()
 #Run tests, print to results files.
-print("Running testrunner.py")
-os.system('python3 -m pytest Tests/testrunner.py > Tests/Results/Runner1.txt')
+os.system('echo "Running testrunner.py"')
+#os.system('pytest "./Tests/Sprint 1/testrunner.py" > Tests/Results/Runner1.txt')
+os.system('pytest "./Tests/testrunner.py" > Tests/Results/Runner1.txt')
 #Get result from file, print to Shell
 temp = subprocess.check_output(['tail', '-1', 'Tests/Results/Runner1.txt'])
 tempstr = str(temp)
@@ -22,10 +30,12 @@ tempstr = str(temp)
 tempstr = tempstr.translate(str.maketrans('', '', '\'=b\\'))
 tempstr = tempstr[1:len(tempstr)-1]
 print(tempstr)
+file1.close()
 
 empty_students()
-print("Finished testrunner.py\n\nRunning testrunner2.py")
-os.system('python3 -m pytest Tests/testrunner2.py > Tests/Results/Runner2.txt')
+os.system('echo "Finished testrunner.py\n\nRunning testrunner2.py"')
+#os.system('pytest "./Tests/Sprint 1/testrunner2.py" > Tests/Results/Runner2.txt')
+os.system('pytest "./Tests/testrunner2.py" > Tests/Results/Runner2.txt')
 #Get result from file, print to Shell
 temp = subprocess.check_output(['tail', '-1', 'Tests/Results/Runner2.txt'])
 tempstr = str(temp)
@@ -33,22 +43,39 @@ tempstr = str(temp)
 tempstr = tempstr.translate(str.maketrans('', '', '\'=b\\'))
 tempstr = tempstr[1:len(tempstr)-1]
 print(tempstr)
+file2.close()
 
+empty_students()
+os.system('echo "Finished testrunner2.py\n\nRunning testrunner3.py"')
+#os.system('pytest "./Tests/Sprint 1/testrunner3.py" > Tests/Results/Runner3.txt')
+os.system('pytest "./Tests/testrunner3.py" > Tests/Results/Runner3.txt')
+#Get result from file, print to Shell
+temp = subprocess.check_output(['tail', '-1', 'Tests/Results/Runner3.txt'])
+tempstr = str(temp)
+#MUST ESCAPE ' CHARACTER FIRST
+tempstr = tempstr.translate(str.maketrans('', '', '\'=b\\'))
+tempstr = tempstr[1:len(tempstr)-1]
+print(tempstr)
+file3.close()
 
 empty_students()
 empty_jobs()
-print("Finished testrunner2.py\n\nRunning testrunner_jobs.py")
-os.system('python3 -m pytest Tests/testrunner_jobs.py > Tests/Results/Jobs.txt')
+os.system('echo "Finished testrunner3.py\n\nRunning testrunner_jobs.py"')
+#os.system('pytest "./Tests/Sprint 2/testrunner_jobs.py" > Tests/Results/Jobs.txt')
+os.system('pytest "./Tests/testrunner_jobs.py" > Tests/Results/Jobs.txt')
 #Get result from file, print to Shell
 temp = subprocess.check_output(['tail', '-1', 'Tests/Results/Jobs.txt'])
 tempstr = str(temp)
 #MUST ESCAPE ' CHARACTER FIRST
 tempstr = tempstr.translate(str.maketrans('', '', '\'=b\\'))
 tempstr = tempstr[1:len(tempstr)-1]
+print(tempstr, "<--- Expected")
+file3.close()
 
 empty_jobs()
-print("Finished testrunner_jobs.py\n\nRunning testrunner_jobs_max.py")
-os.system('python3 -m pytest Tests/testrunner_jobs_max.py > Tests/Results/Jobs_max.txt')
+os.system('echo "Finished testrunner_jobs.py\n\nRunning testrunner_jobs_max.py"')
+#os.system('pytest "./Tests/Sprint 2/testrunner_jobs_max.py" > Tests/Results/Jobs.txt')
+os.system('pytest "./Tests/testrunner_jobs_max.py" > Tests/Results/Jobs_max.txt')
 #Get result from file, print to Shell
 temp = subprocess.check_output(['tail', '-1', 'Tests/Results/Jobs_max.txt'])
 tempstr = str(temp)
@@ -56,7 +83,8 @@ tempstr = str(temp)
 tempstr = tempstr.translate(str.maketrans('', '', '\'=b\\'))
 tempstr = tempstr[1:len(tempstr)-1]
 print(tempstr)
+file3.close()
 
 empty_jobs()
 empty_students()
-print("echo Finished testrunner_jobs.py\n")
+os.system('echo "Finished testrunner_jobs.py\n"')
--- Tests/testrunner_friend_requests.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from io import StringIO
-from App import InCollege
-
-#def test_send_request:
